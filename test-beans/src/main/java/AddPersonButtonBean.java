e245b3ae8344167a7a858d87725c85ec9ec95dac
AnnFedosova <AnnFedosova@yandex.ru>
2019-02-09 16:07:51 +0300

    Добавила кнопки

---
+++ test-beans/src/main/java/AddPersonButtonBean.java
@@ -0,0 +1,55 @@
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+
+@ManagedBean(name = "addPersonButtonBean")
+public class AddPersonButtonBean {
+
+    private MenuModel model;
+
+    @PostConstruct
+    public void init() {
+        model = new DefaultMenuModel();
+
+        //First submenu
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
+
+        DefaultMenuItem item = new DefaultMenuItem("External");
+        item.setUrl("http://www.primefaces.org");
+        firstSubmenu.addElement(item);
+
+        model.addElement(firstSubmenu);
+    }
+
+    public MenuModel getModel() {
+        return model;
+    }
+
+    public void save() {
+        addMessage("Data saved");
+    }
+
+    public void update() {
+        addMessage("Data updated");
+    }
+
+    public void delete() {
+        addMessage("Data deleted");
+    }
+
+    public void buttonAction() {
+        addMessage("Welcome to Primefaces!!");
+    }
+
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
